b5b5b5f8c737f7406085308183b0d1bd5e3b1df8
Tanise Santos <tanise234@gmail.com>
2022-04-14 19:55:14 -0300

    Requisito 2 concluído

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  let isOlder = false;
+  species.forEach((specie) => {
+    if (specie.name === animal) {
+      isOlder = specie.residents.every((resident) => resident.age >= age);
+    }
+  });
+  return isOlder;
 }
 
 module.exports = getAnimalsOlderThan;
