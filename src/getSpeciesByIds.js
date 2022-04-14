52921362fe83a308acc179be59924acf26127a3e
Tanise Santos <tanise234@gmail.com>
2022-04-14 17:36:47 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const result = [];
+  if (ids.length === 0) {
+    return result;
+  }
+  ids.forEach((id) => {
+    result.push(...data.species.filter((specie) => specie.id === id));
+  });
+  return result;
 }
 
 module.exports = getSpeciesByIds;
