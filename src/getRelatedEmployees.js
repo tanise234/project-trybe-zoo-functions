2e90c5a9c20028bc051c1e5364dd27df35c272bc
Tanise Santos <tanise234@gmail.com>
2022-04-18 17:16:18 -0300

    Requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === true) {
+    const staffObject = employees.filter((employee) => employee.managers.includes(managerId));
+    const staffPeople = staffObject.map((staff) => `${staff.firstName} ${staff.lastName}`);
+    return staffPeople;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
