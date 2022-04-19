be6015b96c612088367c4d82c82ca349525a0446
Tanise Santos <tanise234@gmail.com>
2022-04-19 00:31:13 -0300

    REquisito 8 - parte 1 feita

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const specificAnimal = (target) => {
+  return data.species.find((specie) => specie.name === target).availability;
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  return specificAnimal(scheduleTarget);
 }
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
