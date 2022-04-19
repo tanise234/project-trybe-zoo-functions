d507c4f98a76386fcd63bf95659a32400219a6dd
Tanise Santos <tanise234@gmail.com>
2022-04-19 01:42:27 -0300

    Requisito 9 iniciado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+const oldest = (animals) => {
+  return species.find((specie) => specie.name === animals).residents.reduce((oldestAnimal, eachAnimal) => oldestAnimal.age < eachAnimal.age ? oldestAnimal = eachAnimal : oldestAnimal = oldestAnimal);
+}
+
+const animals = data.species[0];
+console.log(oldest(animals));
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const old = oldest(firstSpecie);
+  return [old.name, old.sex, old.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
