f7227ad77fde574fc0004768e67007209d01d5c7
Tanise Santos <tanise234@gmail.com>
2022-04-19 01:14:12 -0300

    Requisito 8 - no meio da parte 2

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,36 @@
-const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
+const arrayDays = Object.keys(data.hours);
+
+const specificDay = (day) => {
+  const opening = arrayDays.find((arrayDay) => arrayDay === day).open;
+  const closing = arrayDays.find((arrayDay) => arrayDay === day).close;
+  const array = [
+    [day],
+    [
+      [
+        [`officeHour`],
+        [`Open from ${opening}am until ${closing}pm`],
+      ],
+      [[`exhibition`], [data.hours.day]],
+    ],
+  ];
+  return Object.fromEntries(array);
+};
 
 const specificAnimal = (target) => {
-  return data.species.find((specie) => specie.name === target).availability;
-}
+  return species.find((specie) => specie.name === target).availability;
+};
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
-  return specificAnimal(scheduleTarget);
+  if (arrayDays.some((day) => day === scheduleTarget)) {
+    return specificDay(scheduleTarget);
+  }
+  if (species.some((specie) => specie.name === scheduleTarget)) {
+    return specificAnimal(scheduleTarget);
+  }
 }
-console.log(getSchedule('lions'));
+console.log(getSchedule("Tuesday"));
 
 module.exports = getSchedule;
