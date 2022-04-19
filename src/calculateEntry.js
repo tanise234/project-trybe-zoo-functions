af6cc3c27f520c972a34f27623c12efb478d79db
Tanise Santos <tanise234@gmail.com>
2022-04-19 00:14:12 -0300

    Requisito 6 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,28 +2,30 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
-  return entrants.reduce((acc, entrant) => {
-    if (entrant.age < 18) {
-      return { child: acc.child + 1,  adult: acc.adult, senior: acc.senior };
-    }
-    if (entrant.age < 50) {
-      return { child: acc.child,  adult: acc.adult + 1, senior: acc.senior };
-    }
-    return { child: acc.child,  adult: acc.adult, senior: acc.senior + 1 };
-  }, { child: 0, adult: 0, senior: 0 });
+  return entrants.reduce(
+    (acc, entrant) => {
+      if (entrant.age < 18) {
+        return { child: acc.child + 1, adult: acc.adult, senior: acc.senior };
+      }
+      if (entrant.age < 50) {
+        return { child: acc.child, adult: acc.adult + 1, senior: acc.senior };
+      }
+      return { child: acc.child, adult: acc.adult, senior: acc.senior + 1 };
+    },
+    { child: 0, adult: 0, senior: 0 },
+  );
 }
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-console.log(countEntrants(entrants));
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const childPrice = countEntrants(entrants).child * data.prices.child;
+  const adultPrice = countEntrants(entrants).adult * data.prices.adult;
+  const seniorPrice = countEntrants(entrants).senior * data.prices.senior;
+
+  return childPrice + adultPrice + seniorPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
