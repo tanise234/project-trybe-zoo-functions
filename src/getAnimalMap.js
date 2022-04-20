603d24ff803ac535008f3f1860e49ba77eaa6b2c
Tanise Santos <tanise234@gmail.com>
2022-04-20 14:26:35 -0300

    Requisito 7 - função passada sem parâmetros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,27 +1,50 @@
-const data = require("../data/zoo_data");
+const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+
+const arrayNE = [];
+const arrayNW = [];
+const arraySE = [];
+const arraySW = [];
+
+const animalsByLocation = () => {
+  species.forEach((specie) => {
+    switch (specie.location) {
+    case 'NE':
+      arrayNE.push(specie.name);
+      break;
+    case 'NW':
+      arrayNW.push(specie.name);
+      break;
+    case 'SE':
+      arraySE.push(specie.name);
+      break;
+    default:
+      arraySW.push(specie.name);
+      break;
+    }
+  });
+  return { NE: arrayNE, NW: arrayNW, SE: arraySE, SW: arraySW };
+};
 
 function getAnimalMap(options) {
   // seu código aqui
-  if (
-    Object.keys(options).includes('includeNames') &&
-    options['includeNames'] === true
-  ) {
-    //
-    console.log("first thing working");
-    if (Object.keys(options).includes('sex') && options['sex'] === "female") {
-      console.log("is female");
-    }
-    //
-    if (Object.keys(options).includes('sex') && options['sex'] === "male") {
-      console.log("is male");
-    }
-    //
-    if (Object.keys(options).includes('sorted') && options['sorted'] === true) {
-      console.log("sorted");
-    }
+  if (!options) {
+    return animalsByLocation();
   }
+  // if (
+  //   Object.keys(options).includes('includeNames') &&
+  //   options.includeNames === true
+  // ) {
+  //   console.log('first thing working');
+  //   if (Object.keys(options).includes('sex') && options.sex === 'female') {
+  //   }
+  //   if (Object.keys(options).includes('sex') && options.sex === 'male') {
+  //   }
+  //   if (Object.keys(options).includes('sorted') && options.sorted === true) {
+  //   }
+  // }
 }
 
-getAnimalMap({ includeNames: true, sorted: false, sex: 'male' });
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
