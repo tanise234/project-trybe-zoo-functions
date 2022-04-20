62e61b5fa3cc26d12a64bbd4c395b0a622c21412
Tanise Santos <tanise234@gmail.com>
2022-04-19 21:16:44 -0300

    Requisito 9 feito - falta passar no lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,16 +1,20 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const oldest = (animals) => {
-  return species.find((specie) => specie.name === animals).residents.reduce((oldestAnimal, eachAnimal) => oldestAnimal.age < eachAnimal.age ? oldestAnimal = eachAnimal : oldestAnimal = oldestAnimal);
-}
-
-const animals = data.species[0];
-console.log(oldest(animals));
+const oldest = (animalsId) => {
+  const arrayAnimals = species.find(
+    (specie) => specie.id === animalsId,
+  ).residents;
+  return arrayAnimals.reduce(
+    (oldestAnimal, eachAnimal) =>
+      oldestAnimal.age < eachAnimal.age ? eachAnimal : oldestAnimal,
+  );
+};
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  const firstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstSpecie = data.employees.find((employee) => employee.id === id)
+    .responsibleFor[0];
   const old = oldest(firstSpecie);
   return [old.name, old.sex, old.age];
 }
