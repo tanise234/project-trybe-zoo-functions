774fbc80467b682e1e66ebb15567ea801ddf2bf9
Tanise Santos <tanise234@gmail.com>
2022-04-20 00:38:24 -0300

    Requisito 7 - reconhece os parâmetros

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (
+    Object.keys(options).includes('includeNames') &&
+    options['includeNames'] === true
+  ) {
+    //
+    console.log("first thing working");
+    if (Object.keys(options).includes('sex') && options['sex'] === "female") {
+      console.log("is female");
+    }
+    //
+    if (Object.keys(options).includes('sex') && options['sex'] === "male") {
+      console.log("is male");
+    }
+    //
+    if (Object.keys(options).includes('sorted') && options['sorted'] === true) {
+      console.log("sorted");
+    }
+  }
 }
+
+getAnimalMap({ includeNames: true, sorted: false, sex: 'male' });
 
 module.exports = getAnimalMap;
