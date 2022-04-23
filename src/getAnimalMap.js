9a14036b9705dd59ff3622720eb48b902fbe11c3
Tanise Santos <tanise234@gmail.com>
2022-04-22 21:29:40 -0300

    Refatoração da função animalsByLocation

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,29 +1,43 @@
-const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
-
-const arrayNE = [];
-const arrayNW = [];
-const arraySE = [];
-const arraySW = [];
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
 const animalsByLocation = () => {
-  species.forEach((specie) => {
-    switch (specie.location) {
-    case 'NE':
-      arrayNE.push(specie.name);
-      break;
-    case 'NW':
-      arrayNW.push(specie.name);
-      break;
-    case 'SE':
-      arraySE.push(specie.name);
-      break;
-    default:
-      arraySW.push(specie.name);
-      break;
-    }
-  });
+  const arrayNE = species.filter((specie) => specie.location === 'NE').map((specie) => specie.name);
+  const arrayNW = species.filter((specie) => specie.location === 'NW').map((specie) => specie.name);
+  const arraySE = species.filter((specie) => specie.location === 'SE').map((specie) => specie.name);
+  const arraySW = species.filter((specie) => specie.location === 'SW').map((specie) => specie.name);
+
   return { NE: arrayNE, NW: arrayNW, SE: arraySE, SW: arraySW };
+};
+
+const animalsByLocationAndName = () => {
+  // species.forEach((specie) => {
+  //   switch (specie.location) {
+  //     case "NE":
+  //       specie.residents.forEach((resident) => arrayNE.push(resident.name));
+  //       break;
+  //     case "NW":
+  //       arrayNW.push(specie.name);
+  //       break;
+  //     case "SE":
+  //       arraySE.push(specie.name);
+  //       break;
+  //     default:
+  //       arraySW.push(specie.name);
+  //       break;
+  //   }
+  // });
+  // return locationObject;
+
+  const residentsByLocation = animalsByLocation().NE.map(
+    (animals) => {
+      [animals] = 
+      species.find((specie) => specie.name === animals).residents
+    }
+  );
+  return residentsByLocation;
+  // .forEach((resident) => arrayNE.push(resident.name))
+  // );
 };
 
 function getAnimalMap(options) {
