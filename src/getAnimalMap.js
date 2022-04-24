aae13a0e8085ec007737f19fb4a684952a2f6c86
Tanise Santos <tanise234@gmail.com>
2022-04-24 18:36:46 -0300

    Requisito 7 -  função sexFilter implementada

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,63 +2,83 @@ const { species } = require("../data/zoo_data");
 const data = require("../data/zoo_data");
 
 const animalsByLocation = () => {
-  const arrayNE = species.filter((specie) => specie.location === 'NE').map((specie) => specie.name);
-  const arrayNW = species.filter((specie) => specie.location === 'NW').map((specie) => specie.name);
-  const arraySE = species.filter((specie) => specie.location === 'SE').map((specie) => specie.name);
-  const arraySW = species.filter((specie) => specie.location === 'SW').map((specie) => specie.name);
-
+  const arrayNE = species
+    .filter((specie) => specie.location === "NE")
+    .map((specie) => specie.name);
+  const arrayNW = species
+    .filter((specie) => specie.location === "NW")
+    .map((specie) => specie.name);
+  const arraySE = species
+    .filter((specie) => specie.location === "SE")
+    .map((specie) => specie.name);
+  const arraySW = species
+    .filter((specie) => specie.location === "SW")
+    .map((specie) => specie.name);
   return { NE: arrayNE, NW: arrayNW, SE: arraySE, SW: arraySW };
 };
 
-const animalsByLocationAndName = () => {
-  // species.forEach((specie) => {
-  //   switch (specie.location) {
-  //     case "NE":
-  //       specie.residents.forEach((resident) => arrayNE.push(resident.name));
-  //       break;
-  //     case "NW":
-  //       arrayNW.push(specie.name);
-  //       break;
-  //     case "SE":
-  //       arraySE.push(specie.name);
-  //       break;
-  //     default:
-  //       arraySW.push(specie.name);
-  //       break;
-  //   }
-  // });
-  // return locationObject;
-
-  const residentsByLocation = animalsByLocation().NE.map(
-    (animals) => {
-      [animals] = 
-      species.find((specie) => specie.name === animals).residents
-    }
+const animalsByLocationAndName = (options) => {
+  return Object.fromEntries(
+    Object.entries(animalsByLocation()).map((keyValueArray) => {
+      return [
+        [keyValueArray[0]],
+        keyValueArray[1].map((animal) => {
+          const arrayNames = [];
+          const arrayResidents = species.find(
+            (specie) => specie.name === animal
+          ).residents;
+          for (let resident of arrayResidents) {
+            sexFilter(options, resident, arrayNames);
+          }
+          return {
+            [animal]: arrayNames,
+          };
+        }),
+      ];
+    })
   );
-  return residentsByLocation;
-  // .forEach((resident) => arrayNE.push(resident.name))
-  // );
 };
+
+const sexFilter = (options, resident, arrayNames) => {
+  if (
+    Object.keys(options).includes("sex") &&
+    options.sex === "female" &&
+    resident.sex === "female"
+  ) {
+    arrayNames.push(resident.name);
+    return arrayNames;
+  }
+  if (
+    Object.keys(options).includes("sex") &&
+    options.sex === "male" &&
+    resident.sex === "male"
+  ) {
+    arrayNames.push(resident.name);
+    return arrayNames;
+  }
+};
+
+const namesSorted = () => {};
 
 function getAnimalMap(options) {
   // seu código aqui
   if (!options) {
     return animalsByLocation();
   }
-  // if (
-  //   Object.keys(options).includes('includeNames') &&
-  //   options.includeNames === true
-  // ) {
-  //   console.log('first thing working');
-  //   if (Object.keys(options).includes('sex') && options.sex === 'female') {
-  //   }
-  //   if (Object.keys(options).includes('sex') && options.sex === 'male') {
-  //   }
-  //   if (Object.keys(options).includes('sorted') && options.sorted === true) {
-  //   }
-  // }
+  if (
+    Object.keys(options).includes("includeNames") &&
+    options.includeNames === true
+  ) {
+    return animalsByLocationAndName(options);
+    //   if (Object.keys(options).includes('sex') && options.sex === 'female') {
+    //   }
+    //   if (Object.keys(options).includes('sex') && options.sex === 'male') {
+    //   }
+    //   if (Object.keys(options).includes('sorted') && options.sorted === true) {
+    //   }
+  }
 }
 
-console.log(getAnimalMap());
+console.log(getAnimalMap({ includeNames: true, sex: "female" }));
 
 module.exports = getAnimalMap;
