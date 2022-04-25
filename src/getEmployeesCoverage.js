9becd652699def7126171e014b0d9ee28f14680b
Tanise Santos <tanise234@gmail.com>
2022-04-24 21:23:26 -0300

    Requisito 10 iniciado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,61 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
-function getEmployeesCoverage() {
+const getEmployeesById = (id) => {
+  const employeeDetails = employees.find((employee) => employee.id === id);
+  if (employeeDetails === undefined) {
+    throw new Error("Informações inválidas");
+  }
+  return employeeDetails;
+};
+
+const getEmployeesByName = (name) => {
+  const employeeDetails = employees.find(
+    (employee) => employee.firstName === name || employee.lastName === name
+  );
+  if (employeeDetails === undefined) {
+    throw new Error("Informações inválidas");
+  }
+  return employeeDetails;
+};
+
+const speciesNames = (employee) =>
+  employee.responsibleFor.map(
+    (animal) => species.find((specie) => specie.id === animal).name
+  );
+
+const speciesLocation = (employee) =>
+employee.responsibleFor.map(
+  (animal) => species.find((specie) => specie.id === animal).location
+);
+
+
+const coverage = (employee) => {
+  return {
+    id: employee.id,
+    fullname: `${employee.firstName} ${employee.lastName}`,
+    species: speciesNames(employee), location: speciesLocation(employee),
+  };
+};
+
+function getEmployeesCoverage(options) {
   // seu código aqui
+  if (!options) {
+    console.log("sem opções especificadas");
+    return;
+  }
+  if (options.name) {
+    const employee = getEmployeesByName(options.name);
+    return coverage(employee);
+  }
+  if (options.id) {
+    const employee = getEmployeesById(options.id);
+    return coverage(employee);
+  }
 }
+
+console.log(
+  getEmployeesCoverage()
+);
 
 module.exports = getEmployeesCoverage;
